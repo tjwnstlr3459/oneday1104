cf3aaf60667b4b616a2e9f870835dacbecf91eef
tjwnstlr3459 <70847393+tjwnstlr3459@users.noreply.github.com>
2021-02-07 22:09:18 +0900

    test2

--- src/Package/TestClass.java
+++ src/Package/TestClass.java
@@ -3,8 +3,9 @@ package Package;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello! this is Github Practicing!");
+		
 	}
 
 }
