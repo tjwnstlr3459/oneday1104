710a06bc34c6218fdb7abc1fa44813c340bb1677
tjwnstlr3459 <70847393+tjwnstlr3459@users.noreply.github.com>
2021-03-08 20:20:28 +0900

    User객체의 직렬화(객체를 바이트화 시켜 스트림에 전송) implements Serializable 내보내기,불러오기

---
+++ src/stream/User.java
@@ -0,0 +1,39 @@
+package stream;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialversionUID = 1111111l;
+	private String name;
+	private int age;
+	private String addr;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String name, int age, String addr) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.addr = addr;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+}
