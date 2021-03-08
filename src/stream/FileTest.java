d48e3b4fef59391ba1e85843ef3d2e1eb6d3f848
tjwnstlr3459 <70847393+tjwnstlr3459@users.noreply.github.com>
2021-03-08 20:18:01 +0900

    object File의 기능(file.exists=존재유무)기 때문에try/catch강제화x (파일명,상대경로,절대경로,파일크기)

---
+++ src/stream/FileTest.java
@@ -0,0 +1,22 @@
+package stream;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileTest {
+	public void fileTest() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("파일명을 입력하세요 : ");
+		String fileName = sc.next();
+		//try/chatch 강제화 x
+		File file = new File(fileName);
+		if(file.exists()) {//exists()-> 해당파일이 존재하면 true/없으면 false
+			System.out.println("파일명 : " + file.getName());
+			System.out.println("파일 상대 경로 : "+file.getPath());
+			System.out.println("파일 절대 경로 : "+file.getAbsolutePath());
+			System.out.println("파일 크기 : "+file.length()+"byte");
+		}else {
+			System.out.println("해당 파일이 존재하지 않습니다.");
+		}
+	}
+}
