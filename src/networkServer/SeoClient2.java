15533a8a82dd64bf674d1155bb995621a632ba97
tjwnstlr3459 <70847393+tjwnstlr3459@users.noreply.github.com>
2021-03-10 17:40:23 +0900

    강사님의 server 퀴즈를 client에 받을수 있도록 수정

--- src/networkServer/SeoClient2.java
+++ src/networkServer/SeoClient2.java
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
 public class SeoClient2 {
 	public void main() {
 		int port = 18080;
-		String addr = "1.220.236.74";
+		String addr = "1.220.236.74";//"172.30.1.7";
 		DataInputStream dis = null;
 		DataOutputStream dos = null;
 		Socket socket = null;
@@ -27,26 +27,42 @@ public class SeoClient2 {
 			dis = new DataInputStream(in);
 			dos = new DataOutputStream(out);
 
-			
-			
-			
-			StringTokenizer st= new StringTokenizer(dis.readUTF(),"\n");
-			while(st.hasMoreTokens()) {
-			String str = dis.readUTF();
-			System.out.println(str);
-			String name = "이윤수";
-			dos.writeUTF(name);
+
+			while(true) {
+				while(true) {
+					String str = dis.readUTF();
+					System.out.println(str);
+					if(str.charAt(str.length()-1)=='?') {
+						System.out.print("나의답변 : ");
+						String myMsg = sc.next();
+						if(myMsg.equals("headset")) {
+							int sum = 0;
+							for (int i = 0; i < myMsg.length(); i++) {
+								char ch = myMsg.charAt(i);
+								sum += (int)ch;
+							}
+							String sum1 = String.valueOf(sum);
+							System.out.println(sum1);
+							dos.writeUTF(sum1);				
+						}else {
+						dos.writeUTF(myMsg);
+						}
+					}
+					break;
+				}
 			}		
-			
-			
-			
-			
-			
-			
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally {
+			try {
+				socket.close();
+				dis.close();
+				dos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
